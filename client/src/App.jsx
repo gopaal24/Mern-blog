ee2894a430581e44c57fdf17f3ddf770de8ab7f5
A Yodha Gopal <gopaal.akkineni@gmail.com>
2024-02-24 12:53:00 +0530

    Created Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,13 +12,13 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
-      <Header className="border-b-2"/>
+      <Header/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/sign-in" element={<SignIn/>}/>
-          <Route path="sign-up/" element={<SignUp/>}/>
-          <Route path="dashboard/" element={<Dashboard/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
         </Routes>
       </BrowserRouter>
     </>
