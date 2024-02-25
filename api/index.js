097a8a088f33bf816edc64a90ccfd7d61b5a5a70
A Yodha Gopal <gopaal.akkineni@gmail.com>
2024-02-25 13:10:05 +0530

    Connected MongoDB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
 
-const app = express()
+mongoose
+  .connect(
+    "mongodb+srv://yodha_akkineni:yodha_akkineni@yodha-blog.tmnu5g1.mongodb.net/blog_DB",
+  )
+  .then(() => {
+    console.log("Succesfully connected");
+  })
+  .catch((err) => {
+    console.log(`Error: ${err}`);
+  });
 
-const port = 5000
+const app = express();
+
+const port = 5000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
