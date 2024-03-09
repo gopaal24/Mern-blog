6a3ddf0c881f63cb200079f382a7d19a514ddc55
A Yodha Gopal <gopaal.akkineni@gmail.com>
2024-03-09 12:27:04 +0530

    hashing password using bcryptjs

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,32 @@
-import User from '../models/user.model.js'
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
 
-  if(!username || !email || !password || username==="" || email === "" || password ===""){
-    return res.status(400).json({message: "All fields are required"})
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
   }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10)
 
   const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
   });
 
-  await newUser.save()
-
+  try {
+    await newUser.save();
+    res.json({ message: "New user successfully created" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
